05cbd784c693729d7470d4d1fdb27d13f48b7932
Aditya Suryawanshi <140580018+codewithAditya07@users.noreply.github.com>
2023-09-07 13:39:09 +0530

    Add files via upload

---
+++ pyramid/halfPyramid.java
@@ -0,0 +1,22 @@
+package pyramid;
+
+public class halfPyramid{
+    public static void main(String[] args) {
+
+        int n=4;
+
+
+        for (int i=0; i<=n;i++){
+            for (int j = 0; j < i; j++) {
+
+
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+
